9a15ef3d5b73be52146b42e68454ea622373d552
TalantDeveloper <139768692+TalantDeveloper@users.noreply.github.com>
2024-06-27 18:38:38 +0500

    save change
    save change

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ app_name = 'base'
 urlpatterns = [
     path('', views.endpoints),
     path('advocates/', views.advocate_list),
+    path('add_advocate/', views.advocate_add),
     path('advocates/<str:username>/', views.advocate_details),
 
 ]
--- base/views.py
+++ base/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import Advocate
 from .serializers import AdvocateSerializer
+from django.db.models import Q
 
 
 @api_view(['GET'])
@@ -14,11 +15,23 @@ def endpoints(request):
 
 @api_view(['GET'])
 def advocate_list(request):
-    data = ['botir', 'shaxzod', 'sobir']
-    advocates = Advocate.objects.all()
+    query = request.GET.get('query')
+
+    if query == None:
+        query = ''
+
+    advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
     serializer = AdvocateSerializer(advocates, many=True)
 
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def advocate_add(request):
+    Advocate.objects.create(
+        username=request.data['username'],
+    )
+    return Response('added')
 
 
 @api_view(['GET', 'POST'])
